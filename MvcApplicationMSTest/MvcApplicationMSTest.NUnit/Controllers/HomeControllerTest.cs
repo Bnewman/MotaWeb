0ffcc7d94998c24d570a1b6ea7318cf1e04d004e
Bnewman <apesma@gmail.com>
2011-10-11 16:15:17 -0700

    Adding NUnit tests
    To see if we can get Bamboo to build them

---
+++ MvcApplicationMSTest/MvcApplicationMSTest.NUnit/Controllers/HomeControllerTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using MvcApplicationMSTest.Controllers;
+using NUnit.Framework;
+
+namespace MvcApplicationMSTest.NUnit
+{
+    [TestFixture]
+    public class HomeControllerTest
+    {
+        [Test]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.AreEqual("Test of the Bamboo CI", result.ViewBag.Message);
+        }
+
+        [Test]
+        public void About()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.About() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+    }
+}
